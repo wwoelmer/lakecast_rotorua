cbc57c28afdffefb554e102d8054128fd19d9512
wwoelmer <wwoelmer@vt.edu>
2025-08-22 15:27:05 +1200

    update script for automation

---
+++ scripts/workflow/05_download_buoy_latest.R
@@ -0,0 +1,35 @@
+# script to download latest buoy data
+
+#devtools::install_github("karthik/rdrop2")
+library(rdrop2)
+
+token <- readRDS("token.rds")
+drop_acc(dtoken = token)  # Or just use functions with `dtoken = token`
+
+
+# name of file once downloaded
+local_file <- './data/buoy/rotorua_profiles_latest.csv'
+
+# list the files in the rotorua folder and select the most recent one
+fls <- drop_dir("bop/Rotorua/")
+fls <- fls %>% 
+  arrange(desc(client_modified))
+fl_name <- as.data.frame(fls[1,2])
+
+# download the most recent file
+rdrop2::drop_download(paste0("bop/Rotorua/", fl_name),
+                      local_path = local_file,
+                      overwrite = TRUE)
+
+
+# now download meteorology
+fl_name_met <- as.data.frame(fls[2,2])
+# name of file once downloaded
+local_file_met <- './data/buoy/rotorua_meteorology_latest.csv'
+
+rdrop2::drop_download(paste0("bop/Rotorua/", fl_name_met),
+                      local_path = local_file_met,
+                      overwrite = TRUE)
+
+
+#file.remove(".httr-oauth")
