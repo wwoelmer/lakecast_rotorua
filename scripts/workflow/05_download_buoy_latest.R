a4ab250f0748a5b1a7f53cfc684de6abdbcb732b
wwoelmer <wwoelmer@vt.edu>
2025-08-25 13:56:48 +1200

    update token

--- scripts/workflow/05_download_buoy_latest.R
+++ scripts/workflow/05_download_buoy_latest.R
@@ -5,14 +5,14 @@ library(rdrop2)
 library(dplyr)
 
 # Read the token from GitHub Actions secret
-token_string <- Sys.getenv("DROPBOX_APP_TOKEN")
+token <- readRDS('token.rds')
 
 
 # name of file once downloaded
 local_file <- './data/buoy/rotorua_profiles_latest.csv'
 
 # list the files in the rotorua folder and select the most recent one
-fls <- drop_dir("bop/Rotorua/", dtoken = token_string)
+fls <- drop_dir("bop/Rotorua/", dtoken = token)
 fls <- fls %>% 
   arrange(desc(client_modified))
 fl_name <- as.data.frame(fls[1,2])
@@ -21,7 +21,7 @@ fl_name <- as.data.frame(fls[1,2])
 rdrop2::drop_download(paste0("bop/Rotorua/", fl_name),
                       local_path = local_file,
                       overwrite = TRUE,
-                      dtoken = token_string)
+                      dtoken = token)
 
 
 # now download meteorology
@@ -32,7 +32,7 @@ local_file_met <- './data/buoy/rotorua_meteorology_latest.csv'
 rdrop2::drop_download(paste0("bop/Rotorua/", fl_name_met),
                       local_path = local_file_met,
                       overwrite = TRUE,
-                      dtoken = token_string)
+                      dtoken = token)
 
 
 #file.remove(".httr-oauth")
