b5264a8df8adf50928be61595e65723557b3d8ed
wwoelmer <wwoelmer@vt.edu>
2025-09-03 11:22:07 +1200

    fix met and profile naming mixup

--- scripts/workflow/05_download_buoy_latest.R
+++ scripts/workflow/05_download_buoy_latest.R
@@ -3,6 +3,7 @@
 #devtools::install_github("karthik/rdrop2")
 library(rdrop2)
 library(dplyr)
+library(stringr)
 
 # Read the token from GitHub Actions secret
 token <- readRDS('token.rds')
@@ -13,9 +14,13 @@ local_file <- './data/buoy/rotorua_profiles_latest.csv'
 
 # list the files in the rotorua folder and select the most recent one
 fls <- drop_dir("bop/Rotorua/", dtoken = token)
-fls <- fls %>% 
-  arrange(desc(client_modified))
-fl_name <- as.data.frame(fls[1,2])
+
+fl_name <- fls %>% 
+  arrange(desc(client_modified)) %>% 
+  filter(str_detect(name, "profiles\\.csv$")) %>% 
+  slice(1)
+
+fl_name <- fl_name[1,2]
 
 # download the most recent file
 rdrop2::drop_download(paste0("bop/Rotorua/", fl_name),
@@ -25,7 +30,13 @@ rdrop2::drop_download(paste0("bop/Rotorua/", fl_name),
 
 
 # now download meteorology
-fl_name_met <- as.data.frame(fls[2,2])
+fl_name_met <- fls %>% 
+  arrange(desc(client_modified)) %>% 
+  filter(str_detect(name, "meteorology\\.csv$")) %>% 
+  slice(1)
+
+fl_name_met <- fl_name_met[1,2]
+
 # name of file once downloaded
 local_file_met <- './data/buoy/rotorua_meteorology_latest.csv'
 
