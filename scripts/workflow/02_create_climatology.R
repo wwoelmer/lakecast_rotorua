5a96b0152673e5d17a71cb32ffb8e15cb6619d2c
wwoelmer <wwoelmer@vt.edu>
2025-10-02 12:23:35 +1300

    cleaning up and documenting climatology workflow

--- scripts/workflow/02_create_climatology.R
+++ scripts/workflow/02_create_climatology.R
@@ -3,6 +3,7 @@
 # OR new file added in that folder with name prefix of boprc_cyano_**
 library(tidyverse)
 
+########## read in biovolume data
 boprc <- read.csv('./data/boprc_cyano/boprc_cyano_2015-01-07_2025-05-26.csv')
 
 boprc <- boprc %>% 
@@ -13,7 +14,14 @@ boprc <- boprc %>%
 # alert levels are based on the combined total of all cyanobacteria)
 boprc <- boprc %>% 
   group_by(Site, SampleDate) %>% 
-  summarise(sum_biovolume = sum(PotentiallyToxicBioVolume))
+  summarise(sum_biovolume = sum(PotentiallyToxicBioVolume)) %>% 
+  mutate(doy = yday(SampleDate), # add week and day of year
+         year = year(SampleDate),
+         week = week(SampleDate))
+
+ggplot(boprc, aes(x = week, y = sum_biovolume, color = as.factor(year))) +
+  geom_point() +
+  facet_wrap(~Site) 
 
 ggplot(boprc, aes(x = sum_biovolume)) +
   geom_histogram()
@@ -21,112 +29,92 @@ ggplot(boprc, aes(x = sum_biovolume)) +
 ggplot(boprc, aes(x = log(sum_biovolume))) +
   geom_histogram()
 
-# convert potentially toxic into warning levels based on guideline
-# thresholds from BOPRC based on biovolume
-boprc <- boprc %>% 
-  mutate(warning = case_when(sum_biovolume < 0.5 ~ 'green',
-                             sum_biovolume > 0.5 & sum_biovolume < 9.99 ~ 'orange',
-                             sum_biovolume > 10 ~ 'red'))
-
-ggplot(boprc, aes(x = as.Date(SampleDate), y = warning, color = warning)) +
-  geom_point() +
-  facet_wrap(~Site) +
-  scale_color_manual(values = c('green', 'orange', 'red', 'gray'))
-
-# add week and day of year
-boprc <- boprc %>% 
-  mutate(doy = yday(SampleDate),
-         year = year(SampleDate),
-         week = week(SampleDate))
-
-ggplot(boprc, aes(x = week, y = warning, color = as.factor(year))) +
-  geom_point() +
-  facet_wrap(~Site) 
-
-ggplot(boprc, aes(x = week, y = sum_biovolume, color = as.factor(year))) +
-  geom_point() +
-  facet_wrap(~Site) 
-
+######### log-transform the data for normality
 #calculate mean and sd of potentially toxic biovolume for each week and site
 # first log transform the data because it is not normally distributed, then take mean/sd
 min_val <- min(boprc$sum_biovolume[boprc$sum_biovolume > 0])
 
-climatology <- boprc %>% 
-  group_by(Site, week) %>% 
-  mutate(mean_raw = mean(sum_biovolume),
-         sd_raw = sd(sum_biovolume)) %>% 
-  mutate(log_biovolume = log(sum_biovolume + min_val)) %>% 
-  mutate(mean = mean(log_biovolume),
-            sd = sd(log_biovolume, na.rm = TRUE)) %>% 
-  distinct(Site, week, .keep_all = TRUE) %>% 
-  select(Site, SampleDate, week, mean_raw, sd_raw, mean, sd)
-         
+boprc <- boprc %>% 
+  mutate(log_biovolume = log(sum_biovolume + min_val)) 
 
-ggplot(climatology, aes(x = week, y = mean)) +
+ggplot(boprc, aes(x = log_biovolume)) +
+  geom_histogram()
+
+########### calculate mean and std for each week/site
+climatology <- boprc %>% 
+  group_by(week, Site) %>% 
+  summarise(mu_log = mean(log_biovolume),
+            sd_log = sd(log_biovolume),
+            n_obs = n()) %>% 
+  filter(n_obs > 2) # only consider weeks with more than 2 obs
+
+ggplot(climatology, aes(x = week, y = mu_log)) +
   geom_point() +
-  geom_errorbar(aes(ymin = mean - sd, ymax = mean + sd), width = 0.5) +
+  geom_errorbar(aes(ymin = mu_log - sd_log, ymax = mu_log + sd_log), width = 0.5) +
   facet_wrap(~Site)
 
-# now pull from a distribution for each doy, then convert into likelihood of red, orange, or green warning
-pred <- climatology %>% 
-  group_by(Site, SampleDate) %>% 
-  mutate(ens_pred = list(rlnorm(1000, meanlog = mean, sdlog = sd)))
+climatology <- climatology %>% 
+  group_by(Site, week) %>% 
+  mutate(lower_CI = exp(qnorm(c(0.05, 0.95), mean = mu_log, sd = sd_log)[1]) - min_val,
+         upper_CI = exp(qnorm(c(0.05, 0.95), mean = mu_log, sd = sd_log)[2]) - min_val,
+         mean_sample = exp(qnorm(c(0.5), mean = mu_log, sd = sd_log)) - min_val)
 
-pred %>% 
-  unnest() %>% 
-  ggplot(aes(x = week, y = mean_raw)) +
+ggplot(climatology, aes(x = week, y = mean_sample)) +
   geom_point() +
-  facet_wrap(~Site, scales = 'free')
+  geom_ribbon(aes(ymin = lower_CI, ymax = upper_CI), alpha = 0.4) +
+  facet_wrap(~Site) +
+  theme_bw() +
+  ylab('Biovolume with 95% CI')
 
+########### create an ensemble distribution for each week/site based on the observed mean and std
+set.seed(123)
+pred <- climatology %>% 
+  group_by(Site, week) %>% 
+  mutate(ens_pred = list(rlnorm(1000, meanlog = mu_log, sdlog = sd_log)))
+
+########## calculate the mean and confidence intervals for the simulated distribution
+pred_long <- pred %>%
+  select(Site, week, ens_pred) %>%
+  unnest(ens_pred) %>% 
+  group_by(Site, week) %>% 
+  summarise(mean_ens = mean(ens_pred),
+            p05 = quantile(ens_pred, 0.05),
+            p95 = quantile(ens_pred, 0.95),)
+
+ggplot(pred_long, aes(x = week, y = mean_ens)) +
+  geom_point() +
+  geom_ribbon(aes(ymin = p05, ymax = p95, alpha = 0.4)) +
+  facet_wrap(~ Site, scales = 'free') +
+  theme_bw()
+
+########## calculate the probability of each warning
 warnings <- pred %>% 
-  rowwise() %>% 
-  mutate(green = mean(ens_pred < 0.5),
-         orange = mean(ens_pred >=0.5 & ens_pred < 10),
-         red = mean(ens_pred >= 10))
+  select(Site, week, ens_pred) %>%
+  unnest(ens_pred) %>% 
+  # classify each ensemble member as falling into which warning level
+  mutate(warnings = case_when(ens_pred < 0.5 ~ "Green",
+                              ens_pred >= 0.5 & ens_pred < 10 ~ 'Orange',
+                              ens_pred >= 10 ~ 'Red')) %>% 
+  group_by(Site, week, warnings) %>%
+  summarise(n = n(), .groups = "drop") %>%
+  group_by(Site, week) %>%
+  # calculate the percent of ensemble members in each warning level
+  mutate(prob = n / sum(n)) %>%
+  select(Site, week, warnings, prob)
 
-t2 <- pred %>% 
-  rowwise() %>% 
-  mutate(green = count(ens_pred < 0.5)/1000)
-
-warnings_long <- warnings %>% 
-  pivot_longer(green:red, values_to = 'probability', names_to = 'warning_level')
-
-ggplot(warnings_long, aes(x = week, y = probability, fill = warning_level)) +
+ggplot(warnings, aes(x = week, y = prob, fill = warnings)) +
   geom_col(position = 'stack') +
   facet_wrap(~Site) +
   scale_fill_manual(values = c('green', 'orange', 'red')) +
   theme_bw()
 
-ggplot(warnings_long, aes(x = week, y = probability, group = warning_level, 
-                          color = warning_level)) +
-  geom_line(size = 1) +
-  facet_wrap(~Site) +
-  scale_color_manual(values = c('green', 'orange', 'red')) +
-  theme_bw()
-
-ggplot(warnings_long, aes(x = probability, fill = Site)) +
-  geom_histogram(position = 'dodge') +
-  facet_wrap(~warning_level) +
-  theme_bw()
-
-warnings_long %>% 
-  filter(warning_level=='red') %>% 
-  ggplot(aes(x = week, y = probability)) +
-  geom_line(size = 2) +
-  facet_wrap(~Site) +
-  geom_hline(yintercept = 0.50) +
-  theme_bw() +
-  ylab('Probability of red warning')
-
 # just check that the probabilities sum to 100
-daily_summary <- warnings_long %>% 
+daily_summary <- warnings %>% 
   group_by(Site, week) %>% 
-  summarise(total_prob = sum(probability, na.rm = TRUE))
+  summarise(total_prob = sum(prob, na.rm = TRUE))
 
-warnings_save <- warnings_long %>% 
-  select(-ens_pred)
 
-write.csv(warnings_save, './forecasts/climatology_values_probabilities.csv', row.names = FALSE)
+write.csv(warnings, './forecasts/climatology_values_probabilities.csv', row.names = FALSE)
 #################################################################################
 # produce spatial persistence forecasts at BOPRC using TALT sites
 
